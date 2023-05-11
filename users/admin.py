13b52ee59b06848a70fda8aa282d3af50493723d
xolmomin <xolmomin@gmail.com>
2023-05-11 12:43:42 +0500

    add TODO tasks

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,12 @@
+import datetime
+
 from ckeditor.widgets import CKEditorWidget
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.forms import forms, CharField
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 from users.models import User
@@ -12,16 +15,24 @@ from users.models import User
 class ArticleForm(forms.Form):
     content = CharField(widget=CKEditorWidget())
 
-# Redis, Rabbitmq
-# cron, celery beat
+
+User.objects.filter(date_joined__gte=datetime.datetime.now(timezone.utc) - datetime.timedelta(hours=24))
+
 
 @admin.register(User)
 class UserModelAdmin(UserAdmin):
-    list_display = ('email', 'is_active')
+    list_display = ('email', 'is_active', 'last_send_24hours')
     ordering = ('id',)
     search_fields = ('email',)
     actions = ['send_message']
     list_per_page = 3
+
+    def last_send_24hours(self, obj):
+        return obj.is_active
+
+    last_send_24hours.boolean = True
+    last_send_24hours.short_description = 'Last send message 24 hours'
+
     def send_message(self, request, queryset):
         form = ArticleForm()
         if 'apply' in request.POST:
